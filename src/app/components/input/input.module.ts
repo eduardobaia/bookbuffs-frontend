2326f08fee4ffa559abc275c1bb50a2fde244fff
eduardobaia <eduardo_baia@msn.com>
2021-12-26 13:03:27 +0000

    add jwt auth structure

---
+++ src/app/components/input/input.module.ts
@@ -0,0 +1,12 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { IonicModule } from '@ionic/angular';
+import { InputComponent } from './input.component';
+
+
+@NgModule({
+  imports: [CommonModule, IonicModule],
+  exports: [InputComponent],
+  declarations: [InputComponent]
+})
+export class InputModule { }
